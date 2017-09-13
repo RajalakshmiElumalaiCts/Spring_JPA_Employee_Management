0af50a13654c532e9d75b4004da0161435bad03a
ragulcs <ragulcs@Ragul>
2017-09-13 15:10:24 -0500

    initial

---
+++ src/main/java/com/jpa/emp/SpringJpaEmployeeManagementApplication.java
@@ -0,0 +1,12 @@
+package com.jpa.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaEmployeeManagementApplication.class, args);
+	}
+}
