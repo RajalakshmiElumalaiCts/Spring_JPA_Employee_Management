9441ae720b72fa9570dadd537224fc0a8ecf84c0
ragulcs <ragulcs@Ragul>
2017-11-13 17:34:04 -0600

    Repository implementation

--- src/main/java/com/jpa/emp/SpringJpaEmployeeManagementApplication.java
+++ src/main/java/com/jpa/emp/SpringJpaEmployeeManagementApplication.java
@@ -2,11 +2,22 @@ package com.jpa.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class SpringJpaEmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaEmployeeManagementApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean configureWebXml(DispatcherServlet dispatcherservlet){
+		ServletRegistrationBean servlet = new ServletRegistrationBean(dispatcherservlet);
+		servlet.addUrlMappings("*.do");
+		return servlet;	
+		
 	}
 }
